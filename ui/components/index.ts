c5f9e0afa540ba4d8781fb2bc9d969c3b6a323eb
onlywiseone <wie323032@gmail.com>
2026-06-25 23:39:08 +0000

    feat(#679): add AnchorSelector component with health scores and auto-select

--- ui/components/index.ts
+++ ui/components/index.ts
@@ -6,3 +6,9 @@
 
 export { ApiRequestPanel } from './ApiRequestPanel';
 export type { ApiRequestPanelProps } from './ApiRequestPanel';
+
+export { AnchorSelector } from './AnchorSelector';
+export type { AnchorSelectorProps, AnchorOption } from './AnchorSelector';
+
+export { TransactionTimeline } from './TransactionTimeline';
+export type { TransactionTimelineProps, TxEvent, TxStatus, TxType } from './TransactionTimeline';
